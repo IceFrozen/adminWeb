6628f839295527af73a3e09190f509afd33f7902
zhipeng9 <zhipeng9@staff.weibo.com>
2017-07-20 18:43:58 +0800

    no message

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   appId:"wxe6aca4dd8bfab4a9",
-  baseURL:"http://lizhipeng.space/api",
-  redirectUri:"http://lizhipeng.space/"
+  baseURL:"http://60.205.212.200/api",
+  redirectUri:"http://60.205.212.200/"
 }
 
 /*
