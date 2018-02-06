21fff7d814d3cfac568a950dcb9acb8174a29fa4
JasonLee <taozi031@163.com>
2018-02-07 03:12:09 +0800

    no message

--- src/views/shuce/NumberTool.vue
+++ src/views/shuce/NumberTool.vue
@@ -1,18 +1,71 @@
 <template>
-  <div>
      <div style="margin: 0px;height: inherit;background: url(static/images/bgshuce.jpg);">
-    {{ datatime }}
-      <x-button type="primary" plain @click.native="showPlugin">选择生日</x-button>
+     <grid style="">
+      <grid-item link="/component/cell" style="border-bottom: 0px ">
+       <!--  <img slot="icon" src="../assets/grid_icon.png"> -->
+      </grid-item>
+      <grid-item :link="{ path: '/component/cell'}" label="">
+            <x-button type="default" plain @click.native="showPlugin">选择生日</x-button>
+      </grid-item>
+      <grid-item link="/component/cell">
+      </grid-item>
+    </grid>
+  <div style="top:10;background-color:aquamarine">
+  asdfasdf
+    <grid >
+      <grid-item link="/component/cell" style="border-bottom: 0px ">
+      </grid-item>
+      <grid-item :link="{ path: '/component/cell'}" label="">
+          <img src="/static/images/logo.png" style="heigth:100%;width:100%;border-bottom: 0px">
+      </grid-item>
+      <grid-item link="/component/cell">
+      </grid-item>
+    </grid>
+    <grid>
+        <div style="display: table-cell; width: 100%;vertical-align: middle;text-align: center;heigth:100%"> 
+          <img src="/static/images/result.png" width="50%" alt="dasdfasdfg"> 
+          <div style="position:absolute;z-indent:2;left:35%;top:15px;"> 数字为</div>
+        </div> 
+    </grid>
+    <grid>
+      <grid-item link="/component/cell" style="border-bottom: 0px ">
+       <!--  <img slot="icon" src="../assets/grid_icon.png"> -->
+      </grid-item>
+      <grid-item :link="{ path: '/component/cell'}" label="">
+          <img src="/static/images/shuce.png" style="heigth:100%;width:100%;border-bottom: 0px">
+      </grid-item>
+      <grid-item link="/component/cell">
+      </grid-item>
+    </grid>
+    <grid>
+        <div style="display: table-cell;position: relative; width: 100%;vertical-align: middle;text-align: center;heigth:100%"> 
+          <img src="/static/images/result.png" width="50%" alt="dasdfasdfg"> 
+          <div style="position:absolute;z-indent:2;left:35%;top:15px;"> 数字为</div>
+        </div> 
+    </grid>
+    <grid>
+      <grid-item link="/component/cell" style="border-bottom: 0px ">
+       <!--  <img slot="icon" src="../assets/grid_icon.png"> -->
+      </grid-item>
+      <grid-item :link="{ path: '/component/cell'}" label="">
+          <img src="/static/images/bggua.png" style="heigth:100%;width:100%;border-bottom: 0px">
+      </grid-item>
+      <grid-item link="/component/cell">
+      </grid-item>
+    </grid>
     </div>
   </div>
 </template>
 <script>
-import { Datetime, Group, XButton } from 'vux'
+import { Datetime, Group, XButton, Grid, GridItem, XImg } from 'vux'
 export default {
   components: {
     Datetime,
     Group,
-    XButton
+    XButton,
+    Grid,
+    GridItem,
+    XImg
   },
   data () {
     return {
@@ -43,5 +96,17 @@ export default {
     height: 100%;
     width: 100%;
     overflow-x: hidden;
+  }
+  .weui-grid:after{
+    border-bottom: 0px;
+    border-top: 0px;
+    border-right: 0px solid #D9D9D9;
+  }
+  .weui-grids:before {
+    border-top: 0px;
+  }
+  .weui-grid:before {
+    border-top: 0px;
+    border-right: 0px solid #D9D9D9;
   }
 </style>
